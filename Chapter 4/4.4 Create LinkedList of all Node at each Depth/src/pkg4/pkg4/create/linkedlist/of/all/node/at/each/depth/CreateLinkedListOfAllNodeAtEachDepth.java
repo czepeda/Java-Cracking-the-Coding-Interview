a4c12daf3dc25cf0b76b76708547470e808d7b23
czepeda <cesarzepeda209@gmail.com>
2016-02-01 11:49:47 -0800

    Chapter 4

---
+++ Chapter 4/4.4 Create LinkedList of all Node at each Depth/src/pkg4/pkg4/create/linkedlist/of/all/node/at/each/depth/CreateLinkedListOfAllNodeAtEachDepth.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4.pkg4.create.linkedlist.of.all.node.at.each.depth;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Given a binary tree, design an algorithm which creates a linked list of all
+ * the nodes at each depth (e.g., if you have a tree with depth D, you'll have D
+ * linked lists).
+ */
+public class CreateLinkedListOfAllNodeAtEachDepth {
+
+    public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
+        ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
+        LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+
+        if (root != null) {
+            current.add(root);
+        }
+
+        while (!current.isEmpty()) {
+            result.add(current);
+            LinkedList<TreeNode> parents = current;
+            current = new LinkedList<TreeNode>();
+
+            for (TreeNode parent : parents) {
+                if (parent.left != null) {
+                    current.add(parent.left);
+
+                }
+
+                if (parent.right != null) {
+                    current.add(parent.right);
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    //TEST----------------------------------
+    public static void main(String[] args) {
+        /*
+         *     4
+         *    / \
+         *   1   5
+         *  / \   \
+         * 2   3   7
+         *      \
+         *       6
+         */
+        TreeNode n1 = new TreeNode(1), n2 = new TreeNode(3),
+                n3 = new TreeNode(5), r = new TreeNode(4);
+        n1.left = new TreeNode(2);
+        n1.right = n2;
+        n2.right = new TreeNode(6);
+        r.left = n1;
+        r.right = n3;
+        n3.right = new TreeNode(7);
+        TreeNode.printTree(r);
+        System.out.println();
+
+        ArrayList<LinkedList<TreeNode>> levels = createLevelLinkedList(r);
+        for (LinkedList<TreeNode> level : levels) {
+            for (TreeNode node : level) {
+                System.out.print(node + " ");
+            }
+            System.out.println();
+        }
+    }
+}
