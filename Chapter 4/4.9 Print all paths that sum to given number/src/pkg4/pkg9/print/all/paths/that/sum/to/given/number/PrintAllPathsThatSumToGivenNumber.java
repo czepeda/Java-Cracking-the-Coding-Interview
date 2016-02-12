ff36cc01f59c4b167125a168cc837fc1b4896307
czepeda <cesarzepeda209@gmail.com>
2016-02-12 15:23:39 -0800

    Chapter 4

---
+++ Chapter 4/4.9 Print all paths that sum to given number/src/pkg4/pkg9/print/all/paths/that/sum/to/given/number/PrintAllPathsThatSumToGivenNumber.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4.pkg9.print.all.paths.that.sum.to.given.number;
+
+/**
+ *
+ * @author czepeda
+ */
+import java.util.LinkedList;
+
+/**
+ * You are given a binary tree in which each node contains a value. Design an
+ * algorithm to print all paths which sum to a given value. The path does not
+ * need to start or end at the root or a leaf.
+ */
+public class PrintAllPathsThatSumToGivenNumber {
+
+    public static LinkedList<LinkedList<TreeNode>> findSumPaths(TreeNode root, int target) {
+        LinkedList<LinkedList<TreeNode>> result = new LinkedList<LinkedList<TreeNode>>();
+        findSumPaths(root, target, new LinkedList<TreeNode>(), result);
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void findSumPaths(TreeNode n, int target, LinkedList<TreeNode> path, LinkedList<LinkedList<TreeNode>> result) {
+        if (n == null) {
+            return;
+        }
+
+        path.add(n);
+        int sum = 0;
+        LinkedList<TreeNode> nodes = new LinkedList<TreeNode>();
+
+        for (int i = path.size() - 1; i >= 0; i--) {
+            TreeNode node = path.get(i);
+            nodes.add(node);
+            sum += node.value;
+
+            if (sum == target) {
+                result.add((LinkedList<TreeNode>) nodes.clone());
+            }
+        }
+
+        findSumPaths(n.left, target, path, result);
+        findSumPaths(n.right, target, path, result);
+
+        path.removeLast();
+
+    }
+
+    //TEST----------------------------------
+    public static void main(String[] args) {
+        /*
+         *     4
+         *    / \
+         *   5   2
+         *  / \   \
+         * 1   3   7
+         * \   /  /
+         *  8 6  9
+         */
+        TreeNode n1 = new TreeNode(1), n2 = new TreeNode(2), n3 = new TreeNode(3),
+                n4 = new TreeNode(4), n5 = new TreeNode(5), n6 = new TreeNode(6),
+                n7 = new TreeNode(7), n8 = new TreeNode(8), n9 = new TreeNode(9);
+        n4.left = n5;
+        n4.right = n2;
+        n5.left = n1;
+        n5.right = n3;
+        n1.right = n8;
+        n3.left = n6;
+        n2.right = n7;
+        n7.left = n9;
+        //TreeNode.printTree(n4);
+        System.out.println();
+        LinkedList<LinkedList<TreeNode>> result = findSumPaths(n4, 9);
+        printResult(result);
+        result = findSumPaths(n4, 6);
+        printResult(result);
+    }
+
+    private static void printResult(LinkedList<LinkedList<TreeNode>> result) {
+        for (LinkedList<TreeNode> path : result) {
+            for (TreeNode n : path) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
